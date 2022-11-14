819b55d85893ced5f2dc80ac04cba6c32fa0bb4c
xiabenwei <summer_xbw@163.com>
2022-11-14 21:49:54 +0800

    主题引入

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,18 @@ module.exports = {
   title: "VuePress",
   description: "Just playing around",
   base: "/note-blog/",
-  head: [["link", { rel: "icon", href: "/logo.jpg" }]],
+  head: [
+    [
+      "meta",
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
+    ["link", { rel: "icon", href: "/assets/logo.jpg" }],
+  ],
+  theme: "reco",
+  themeConfig: {
+    logo: "/assets/logo.jpg",
+  },
 };
